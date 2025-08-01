7c786123de0e9e85330e9a6405c83f036bf00ea3
Pierre Marshall <pierre.marshall@gmail.com>
2025-08-01 23:15:43 +0100

    handle all ofher errors with skipping
    should return proper errors from this!

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,16 @@ use std::thread::sleep;
 use std::time::Duration;
 
 use reqwest::header::RETRY_AFTER;
-use reqwest::{Client, Response, StatusCode};
+use reqwest::{Client, StatusCode};
 
 #[tokio::main]
 async fn main() {
     if let Ok(urls) = read_lines("urls") {
         let mut rng = rand::rng();
-        let mut delay_ratchet: u64 = 20u64;
+        let mut delay_ratchet: u64 = 50u64;
 
         for url in urls.map_while(Result::ok).enumerate() {
-            let delay: u64 = rng.random_range(delay_ratchet..(delay_ratchet + 20));
+            let delay: u64 = rng.random_range(delay_ratchet..(delay_ratchet + 50));
             // maintain state by writing line number to file
             let state: usize = fs::read_to_string("state").unwrap().parse().unwrap();
             // skip over line numbers until the last one saved to file
@@ -24,25 +24,40 @@ async fn main() {
                 fs::write("state", url.0.to_string()).unwrap();
 
                 println!("downloading {}", url.1);
-                let page_response: (String, bool) = download_page(&url.1).await;
-                if page_response.1 {
-                    delay_ratchet += rng.random_range(5..20)
-                }
-                let page_file_path = format!("html/{state}.html");
-                fs::write(page_file_path, page_response.0).unwrap();
-                println!("sleeping {delay}ms");
-                sleep(Duration::from_millis(delay));
+                match download_page(&url.1).await {
+                    Ok(response) => {
+                        if response.1 {
+                            delay_ratchet += rng.random_range(40..60)
+                        }
+                        let page_file_path = format!("html/{state}.html");
+                        fs::write(page_file_path, response.0).unwrap();
+                        println!("sleeping {delay}ms");
+                        sleep(Duration::from_millis(delay));
+                    }
+                    Err(err) => {
+                        println!("{err}");
+                        continue;
+                    }
+                };
 
                 let json_url: String = format!("{}/data.json", &url.1);
                 println!("downloading {json_url}");
-                let json_response: (String, bool) = download_page(&json_url).await;
-                if json_response.1 {
-                    delay_ratchet += rng.random_range(5..20)
-                }
-                let json_file_path = format!("json/{state}.json");
-                fs::write(json_file_path, json_response.0).unwrap();
-                println!("sleeping {delay}ms");
-                sleep(Duration::from_millis(delay));
+
+                match download_page(&json_url).await {
+                    Ok(response) => {
+                        if response.1 {
+                            delay_ratchet += rng.random_range(40..60)
+                        }
+                        let json_file_path = format!("json/{state}.json");
+                        fs::write(json_file_path, response.0).unwrap();
+                        println!("sleeping {delay}ms");
+                        sleep(Duration::from_millis(delay));
+                    }
+                    Err(err) => {
+                        println!("{err}");
+                        continue;
+                    }
+                };
             } else {
                 continue;
             }
@@ -70,17 +85,18 @@ fn create_client() -> Client {
         .unwrap()
 }
 
-async fn download_page(url: &String) -> (String, bool) {
+async fn download_page(url: &String) -> Result<(String, bool), String> {
     const RETRY_SCALE: [u64; 13] = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377];
 
     let mut retries: usize = 0;
 
     let client: Client = create_client();
 
-    let response_from_retry: Response = loop {
-        match client.get(url).send().await {
+    let response_from_retry = loop {
+        let response = client.get(url).send().await;
+        match response {
             // if response is successful, return the response!
-            Ok(resp) if resp.status().is_success() => break resp,
+            Ok(resp) if resp.status().is_success() => break Ok(resp),
             // if status is 429, back off and retry
             Ok(resp)
                 if resp.status() == StatusCode::TOO_MANY_REQUESTS
@@ -102,7 +118,7 @@ async fn download_page(url: &String) -> (String, bool) {
                 if let Some(retry_after) = resp.headers().get(RETRY_AFTER)
                     && let Ok(retry_after) = retry_after.to_str()
                     && let Ok(retry_after) = retry_after.parse::<u64>()
-                    && retry_after < 233
+                    && retry_after < 377
                 {
                     let backoff = Duration::from_secs(retry_after + 1);
                     println!("Got a retry-after response, sleeping {backoff:?} seconds");
@@ -110,13 +126,27 @@ async fn download_page(url: &String) -> (String, bool) {
                 }
                 retries += 1;
             }
+            Ok(resp) => {
+                if resp.status().is_server_error() || resp.status().is_client_error() {
+                    let error_message = format!("Got a {} error, skipping", resp.status().as_str());
+                    break Err(error_message);
+                }
+            }
             // Breaking out with an error is fine,
-            // the last match arm should never be met
-            _ => panic!("Network request failed"),
+            // the last match arm should hopefully never be met
+            _ => {
+                println!("network failure")
+            }
         }
     };
 
-    let retried: bool = if retries != 0 { true } else { false };
+    let retried: bool = retries != 0;
 
-    (response_from_retry.text().await.unwrap(), retried)
+    match response_from_retry {
+        Ok(response) => {
+            let response_body = response.text().await.unwrap();
+            Ok((response_body, retried))
+        }
+        Err(error) => Err(error),
+    }
 }
